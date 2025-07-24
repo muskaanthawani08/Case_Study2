44159055fef4221a82fb4e4015685ef075b51c20
Muskaan Thawani <muskaan.thawani@tresvista.com>
2025-07-24 15:57:47 +0530

    final push

--- dags/ecommerce_etl_dag.py
+++ dags/ecommerce_etl_dag.py
@@ -23,18 +23,45 @@ dag = DAG(
 )
 
 start = EmptyOperator(task_id='start', dag=dag)
-end = EmptyOperator(task_id='end', dag=dag)
+skip_alert = EmptyOperator(task_id='end', dag=dag)
+end = EmptyOperator(task_id='completed', dag=dag)
 
-extract_product = PythonOperator(task_id='extract_product_data', python_callable=extract_product_data, dag=dag)
-extract_sales = PythonOperator(task_id='extract_sales_data', python_callable=extract_sales_data, dag=dag)
-extract_feedback = PythonOperator(task_id='extract_feedback_data', python_callable=extract_feedback_data, dag=dag)
+extract_product = PythonOperator(
+    task_id='extract_product_data', 
+    python_callable=extract_product_data, 
+    dag=dag)
 
-transform = PythonOperator(task_id='transform_and_join', python_callable=transform_and_join, dag=dag)
-quality_check = BranchPythonOperator(task_id='run_data_quality_checks', python_callable=run_data_quality_checks, dag=dag)
+extract_sales = PythonOperator(
+    task_id='extract_sales_data', 
+    python_callable=extract_sales_data, 
+    dag=dag)
 
-load = PythonOperator(task_id='load_to_snowflake', python_callable=load_to_snowflake, dag=dag)
-alert = PythonOperator(task_id='send_alert', python_callable=send_alert, dag=dag)
+extract_feedback = PythonOperator(
+    task_id='extract_feedback_data', 
+    python_callable=extract_feedback_data, 
+    dag=dag)
+
+transform = PythonOperator(
+    task_id='transform_and_join', 
+    python_callable=transform_and_join, 
+    dag=dag)
+
+quality_check = BranchPythonOperator(
+    task_id='run_data_quality_checks', 
+    python_callable=run_data_quality_checks, 
+    dag=dag)
+
+load = PythonOperator(
+    task_id='load_to_snowflake', 
+    python_callable=load_to_snowflake, 
+    dag=dag)
+
+alert = PythonOperator(
+    task_id='send_alert', 
+    python_callable=send_alert, 
+    dag=dag)
+
 
 start >> extract_product >> extract_sales >> extract_feedback >> transform >> quality_check
 quality_check >> load >> end
-quality_check >> alert >> end
+quality_check >> alert >> skip_alert
